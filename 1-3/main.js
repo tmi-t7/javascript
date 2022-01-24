9fef9caf3ea5233d90d1fbfa4761be63c79468a3
tmi-t7 <91792000+tmi-t7@users.noreply.github.com>
2022-01-24 19:14:05 +0900

    課題提出です

---
+++ 1-3/main.js
@@ -0,0 +1,24 @@
+let a = 9;
+let b = 12;
+
+if (a != 10) {
+    console.log(a);
+}
+
+if (10 <= b < 20 && b % 2 === 0) {
+    console.log(b);
+}
+
+let x = 10;
+if (10 <= x <=20 ){
+    console.log("成功です");
+} else {
+    console.log("失敗です");
+}
+
+let y = 0;
+if (y % 2 === 0) {
+    console.log("偶数です");
+} else {
+    console.log("奇数です");
+}
