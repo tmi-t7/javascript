be06f96bc79c356012ec0851923dbc82d886277f
tmi-t7 <91792000+tmi-t7@users.noreply.github.com>
2022-01-24 17:19:07 +0900

    課題提出

---
+++ 1-1/main.js
@@ -0,0 +1,10 @@
+console.log("Hello World!!");
+console.log(65536);
+console.log(10+5);
+console.log(10*5);
+
+window.alert("hello world");
+
+console.log("10+8");
+console.log(10+8);
+console.log("20割る3の余りは"+20%3);
