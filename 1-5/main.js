694bb684e035b4705e579091da06db018632ce7a
tmi-t7 <91792000+tmi-t7@users.noreply.github.com>
2022-01-28 21:15:00 +0900

    課題提出です

---
+++ 1-5/main.js
@@ -0,0 +1,10 @@
+function createjuice (fruits) {
+    console.log(fruits + "を受け取りました。ジュースにして返します");
+    return fruits + "ジュース";
+}
+
+let orangejuice = createjuice("みかん");
+    console.log( orangejuice + "が届きました");
+
+// let applejuice = createjuice("りんご");
+    // console.log( applejuice + "が届きました");
