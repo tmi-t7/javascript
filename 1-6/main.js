cf077dbf7b5ca84bf067a5c2f670af984243422c
tmi-t7 <91792000+tmi-t7@users.noreply.github.com>
2022-01-29 02:45:49 +0900

    提出課題です

---
+++ 1-6/main.js
@@ -0,0 +1,16 @@
+let scores = [10, 15,20,25];
+console.log(scores);
+for(let number = 0; number < scores.length; number++) {
+    if(scores [number] % 2 === 0) {
+        console.log(scores[number] + "は偶数です");
+    }
+}
+
+let car = {
+    gass: 20.5,
+    number: 1234,
+};
+    console.log(car);
+    console.log("ガソリンは" + car.gass + " です");
+    console.log("ナンバーは" + car.number + "です");
+
